448edeb38320ca84c1a2e0109a2498ac719b8a58
eotles <eotles@dyn-72-33-238-184.uwnet.wisc.edu>
2014-06-12 11:53:11 -0500

    Updates

--- maxLine.c
+++ maxLine.c
@@ -9,24 +9,52 @@
 #include <stdio.h>
 #define MAXLINE 1000 //max input size
 
-int getline(char line[], int maxline);
+int my_getline(char line[], int maxline);
 void copy(char to[], char from[]);
+void dispArr(char s[]);
 
 //print longest input line
 int main(){
     int len, max;
-    char line[MAXLINE], longest[MAXLINE]
+    char line[MAXLINE], longest[MAXLINE];
     
     max = 0;
-    while((len = getline(line, MAXLINE))>0 )
+    while((len = my_getline(line, MAXLINE)) > 0)
         if (len > max) {
             max = len;
             copy(longest, line);
         }
     if (max > 0) {
-        printf("%s", longest);
+        printf("\nEND\n%s", longest);
+    }
+    return(0);
+}
+
+//my_getline
+//read a line into s, return length
+int my_getline(char s[], int lim){
+    int c, i;
+    
+    for (i=0; i < lim-1 && (c=getchar())!='Q' && c!='\n'; ++i)
+        s[i] = c;
+    if (c=='\n') {
+        s[i]=c;
+        ++i;
+    }
+    s[i] = '\0';
+    return(i);
+}
+
+//copy
+//copy from into to, assuming to is big enough
+void copy(char to[], char from[]){
+    int i;
+    
+    i=0;
+    while ((to[i] = from[i]) != '\0') {
+        printf("%c", to[i]);
+        i++;
     }
 }
 
-//
 
