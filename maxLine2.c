b773418b8fff88ea423948cfff1c66b285421dda
eotles <eotles@dyn-72-33-238-184.uwnet.wisc.edu>
2014-06-12 13:52:44 -0500

    Update

--- maxLine2.c
+++ maxLine2.c
@@ -9,23 +9,26 @@
 #include <stdio.h>
 #define MAXLINE 1000 //max input size
 
+//Test
+
 int max;
 char line[MAXLINE];
 char longest[MAXLINE];
 
-int my_getline(char line[], int maxline);
-void copy(char to[], char from[]);
+int my_getline(void);
+void copy(void);
 
 //print longest input line
 int main(){
-    int len, max;
-    char line[MAXLINE], longest[MAXLINE];
+    int len;
+    extern int max;
+    extern char longest[MAXLINE];
     
     max = 0;
-    while((len = my_getline(line, MAXLINE)) > 0)
+    while((len = my_getline()) > 0)
         if (len > max) {
             max = len;
-            copy(longest, line);
+            copy();
         }
     if (max > 0) {
         printf("\nEND\n%s", longest);
@@ -35,27 +38,28 @@ int main(){
 
 //my_getline
 //read a line into s, return length
-int my_getline(char s[], int lim){
+int my_getline(void){
     int c, i;
+    extern char line[];
     
-    for (i=0; i < lim-1 && (c=getchar())!='Q' && c!='\n'; ++i)
-        s[i] = c;
+    for (i=0; i < MAXLINE-1 && (c=getchar())!='Q' && c!='\n'; ++i)
+        line[i] = c;
     if (c=='\n') {
-        s[i]=c;
+        line[i]=c;
         ++i;
     }
-    s[i] = '\0';
+    line[i] = '\0';
     return(i);
 }
 
 //copy
 //copy from into to, assuming to is big enough
-void copy(char to[], char from[]){
+void copy(void){
     int i;
+    extern char line[], longest[];
     
     i=0;
-    while ((to[i] = from[i]) != '\0') {
-        printf("%c", to[i]);
+    while ((longest[i] = line[i]) != '\0') {
         i++;
     }
 }
